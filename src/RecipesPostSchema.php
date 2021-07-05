d98e3134d00d88608386c5aa36c270110be9725e
danni-stark <dani.m@vanillaforums.com>
2021-07-05 14:41:36 -0400

    Fix setting default values

--- src/RecipesPostSchema.php
+++ src/RecipesPostSchema.php
@@ -26,13 +26,13 @@ class RecipesPostSchema {
         if (empty(self::$schema)) {
             self::$schema = new Assert\Collection([
                 'name' => [new Assert\Length(['min' => 3])],
-                'prepTime' =>  new Assert\Optional([new Assert\Length(['min' => 3])]),
-                'cookingTime' => new Assert\Optional([new Assert\Length(['min' => 3])]),
+                'prepTime' =>  new Assert\Optional([new Assert\Length(['min' => 2])]),
+                'cookingTime' => new Assert\Optional([new Assert\Length(['min' => 2])]),
                 'servings' => new Assert\Optional(),
                 'category' => [new Assert\Length(['min' => 3])],
                 'directions' => [new Assert\Length(['min' => 3])],
                 'favourites' => [new Assert\Optional([new Assert\PositiveOrZero()])],
-                'addedBy' => new Assert\Optional([new Assert\Length(['min' => 3])]),
+                'addedBy' => new Assert\Optional([new Assert\Length(['min' => 2])]),
                 'calories' => new Assert\Optional([new Assert\Length(['min' => 1])]),
                 'cuisine' => new Assert\Optional([new Assert\Length(['min' => 3])]),
                 'ingredients' => [new Assert\Length(['min' => 3])],
